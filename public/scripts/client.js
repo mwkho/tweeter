b6df71e85b0686fc9e5ddd634dc4414f8fa07355
mwkho <martin.h@hotmail.ca>
2021-03-24 20:21:08 +0000

    Make the renderTweet function and refactor html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,9 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-
-
 const dateConvert = (time) => {
   let postedDuration = new Date(time);
   const difference = Date.now() - postedDuration;
@@ -47,28 +44,46 @@ let createTweetElement = (tweet) => {
             <p> Posted ${dateConvert(tweet.created_at)}</p>
             <img src="../images/profile-hex.png"> 
           </footer>
-        </article>`
+        </article>`;
   return formattedTweet;
 }
 
+// Function to format tweet data array and add to the tweet container
+const renderTweets = (tweets) => {
+  tweets.forEach((tweet) => {
+    $('#tweet-container').append(createTweetElement(tweet));
+  })
+}
 
-$(document).ready(function() {
-
-  const tweetData = {
+// Fake data taken from initial-tweets.json
+const data = [
+  {
     "user": {
       "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
     "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
     "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   }
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  $('#tweet-container').append($tweet);
-  
+]
+
+
+$(document).ready(function() {
+  renderTweets(data);
 
   $('article.tweet').hover(function() {
     $(this).addClass('emphasis');
