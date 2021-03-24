15bbc8a16f9c042106ce0aa1361aa4a19c31d75f
mwkho <martin.h@hotmail.ca>
2021-03-24 03:14:13 +0000

    Change the opacity of text when hovered

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,12 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  $('.tweet-container').hover(function() {
+    $(this).addClass('emphasis');
+  } , function() {
+      $(this).removeClass('emphasis')
+
+    }
+  );  
+});
